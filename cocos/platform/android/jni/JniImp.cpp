43f169e734323deb04c2e6faeb511037055e9e1a
王贺天 <hetian@hetian.me>
2020-06-23 10:00:00 +0800

    增加对源码密钥的配置

--- cocos/platform/android/jni/JniImp.cpp
+++ cocos/platform/android/jni/JniImp.cpp
@@ -173,12 +173,17 @@ extern "C"
 	 * Cocos2dxRenderer native functions implementation.
 	 *****************************************************/
 
-    JNIEXPORT void JNICALL JNI_RENDER(nativeInit)(JNIEnv*  env, jobject thiz, jint w, jint h, jstring jDefaultResourcePath)
+    JNIEXPORT void JNICALL JNI_RENDER(nativeInit)(JNIEnv*  env, jobject thiz, jint w, jint h, jstring pass, jstring jDefaultResourcePath)
     {
         g_width = w;
         g_height = h;
+
+        // 销毁FileUtils单例
+        FileUtils::destroyInstance();
         
         g_app = cocos_android_app_init(env, w, h);
+        // 慧知科技
+        g_app->setPass(JniHelper::jstring2string(pass));
 
         g_isGameFinished = false;
         ccInvalidateStateCache();
@@ -464,10 +469,13 @@ extern "C"
         g_apkPath = JniHelper::jstring2string(apkPath);
     }
 
-    JNIEXPORT void JNICALL JNI_HELPER(nativeSetContext)(JNIEnv*  env, jobject thiz, jobject context, jobject assetManager)
+    // 慧知科技删除 取消 jobject context
+    // JNIEXPORT void JNICALL JNI_HELPER(nativeSetContext)(JNIEnv*  env, jobject thiz, jobject context)
+    JNIEXPORT void JNICALL JNI_HELPER(nativeSetContext)(JNIEnv*  env, jobject thiz)
     {
         JniHelper::setClassLoaderFrom(context);
-        FileUtilsAndroid::setassetmanager(AAssetManager_fromJava(env, assetManager));
+        // 慧知科技 删除
+        // FileUtilsAndroid::setassetmanager(AAssetManager_fromJava(env, assetManager));
     }
 
     JNIEXPORT void JNICALL JNI_HELPER(nativeSetAudioDeviceInfo)(JNIEnv*  env, jobject thiz, jboolean isSupportLowLatency, jint deviceSampleRate, jint deviceAudioBufferSizeInFrames)
